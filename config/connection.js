84b6a23856ef9da6ee09d37607717704da3cc314
Majid <tomajid@yahoo.com>
2018-01-13 00:34:42 -0800

    updated JAWSDB_URL params

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ var Sequelize = require("sequelize");
 
 if (process.env.JAWSDB_URL) {
   // Creates mySQL connection using Sequelize
-      var sequelize = new Sequelize("burgers_db", "root", "Tabestan_20", {
-      host: "process.env.JAWSDB_URL",
+      var sequelize = new Sequelize("kr2dxeuk8g19yzgy", "goirsv0qam8zj23e", "ytxvww6ewbg1zj0h", {
+      host: "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       dialect: "mysql",
       pool: {
         max: 5,
